34c526afe3a06a0d6d7c73131508328ba43c7cd7
mirek_sftw <sftwmail@gmail.com>
2024-07-02 18:17:21 +0200

    Add project files.

---
+++ CarWorkshop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarWorkshop.MVC.Models;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+namespace CarWorkshop.MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        var model = new About()
+        {
+            Title = "Warsztat samochodowy",
+            Description = "Pierwsza aplikacja webowa",
+            Tags = new List<string>() { "car", "app", "free" }
+        };
+
+        
+
+        return View(model);
+    }
+
+    public IActionResult Privacy()
+    {
+        var model = new List<Person>()
+        {
+            new Person()
+            {
+                FirstName = "Konrad",
+                LastName = "Mirek"
+            },
+            new Person()
+            {
+                FirstName = "£ukasz",
+                LastName = "Mirek"
+            }
+        };
+
+        return View(model);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
