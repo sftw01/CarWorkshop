dfffd249324383c0beb69c9eb16bca4eea40969f
mirek_sftw <sftwmail@gmail.com>
2024-07-02 20:11:23 +0200

    l 17, modela danych

---
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Domain.Entities
+{
+    public class CarWorkshop
+    {
+        public required int Id { get; set; }        
+
+        public string Name { get; set; } = default!;        // Name is required
+        public string? Description { get; set; }            // Description is optional
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public CarWorkshopContactDetails ContactDetails { get; set; } = default!;       //
+
+        public string EncodedName { get; private set; } = default!;         //used for SEO purposes 
+
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ", "-");    //method to encode the name for SEO purposes
+
+    }
+}
