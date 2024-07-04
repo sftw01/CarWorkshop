6b586e67a67e1a3fd07f8356c2975855b1cbb3e9
mirek_sftw <sftwmail@gmail.com>
2024-07-04 17:38:08 +0200

    l. 20

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.Infrastructure.Persistence
+{
+    internal class CarWorkshopDbContext : DbContext
+    {
+        public DbSet<CarWorkshop.Domain.Entities.CarWorkshop> CarWorkshops { get; set; }    // DbSet for CarWorkshop entity - used to query and save instances of CarWorkshop entity
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)   //
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshopDb;Trusted_Connection=True;");
+        }
+        
+        //ponizsza metoda słuzy do polaczenia z contactDetails, contactDetails nie jest zadna tabela nowa ale nalezy do tabeli Workshop
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+                .OwnsOne(c => c.ContactDetails);
+        }
+    }
+}
