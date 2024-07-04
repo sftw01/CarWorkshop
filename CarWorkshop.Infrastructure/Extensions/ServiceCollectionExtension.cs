ff4b06e69862e5a4b38937a5c28e046ecb0f3f41
mirek_sftw <sftwmail@gmail.com>
2024-07-04 18:01:28 +0200

    l 21 przeniesienie logiki add services do modulu zawierajacego infrastruktute Extensions/ServiceCollectionExtension.cs

---
+++ CarWorkshop.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarWorkshop.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarWorkshop.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CarWorkshopDbContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("CarWorkshop")));             // Add CarWorkshopDbContext to the services container, get connection string from appsettings.json
+        }
+    }
+}
